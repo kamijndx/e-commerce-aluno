c3dc24bd4ec46e5f7ec5fd6b4386c00fabd5022b
kamilly alessandra da costa ferreira <kamillydixon@gmail.com>
2026-08-03 21:17:03 -0300

    corrige interceptor http: catchError retornando throwError

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,10 +1,11 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 import { tap } from "rxjs";
+import { catchError } from "rxjs";
+import { throwError } from "rxjs";
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
     
     console.log('Inteceptando Requisição: ', req.url);
-    
     //! Aqui você pode adicionar lógica para modificar a requisição antes de enviá-la, como adicionar cabeçalhos, tokens de autenticação, etc.
     const token = 'fake-token-jwt';
     
@@ -18,8 +19,21 @@ export const httpInterceptor: HttpInterceptorFn = (req, next) => {
             next: (event) => console.log('Responde: ', event),
             error: (error) => console.error('Erro de Requisição: ', error)
         }),
+        catchError((error) =>{
 
-    
+            console.error('Erro de Requisição Global:',error);
 
+            if(error.status ===401){
+
+            console.warn('erro de autenticaçao de usuario');
+
+            }
+
+            if (error.status === 500){
+
+            console.warn('Erro interno do servidor', error);
+            }
+            return throwError(() => error);
+        }),
     );
 };
