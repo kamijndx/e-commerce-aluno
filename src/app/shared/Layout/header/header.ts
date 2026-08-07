40c9cdc895419a1c96a45439702559ebfee54494
kamilly alessandra da costa ferreira <kamillydixon@gmail.com>
2026-08-06 21:07:29 -0300

    adiciona header, atualiza layout de produtos e configurações do projeto

---
+++ src/app/shared/Layout/header/header.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink, MatToolbarModule, MatButtonModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  nomeLoja='Pezao Tec'
+}
