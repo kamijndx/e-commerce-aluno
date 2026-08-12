e5d93a756f37c4c38b9cad34c3ffe06d37f994c5
Kamilly Ferreira <kamillydixon@gmail.com>
2026-08-12 20:29:52 -0300

    corrige limpar carrinho e formatação de preço no checkout

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,12 +1,16 @@
 
 import { Injectable, signal, computed } from '@angular/core';
 
+type ItemCarrinho = {
+  nome: string;
+  preco: number;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CarrinhoService {
 
-  private carrinho = signal<{nome: string; preco: number}[]>([]);
+  private carrinho = signal<ItemCarrinho[]>([]);
   itens = this.carrinho.asReadonly();
 
   quantidadeItens = computed(() => this.carrinho().length);
@@ -15,7 +19,13 @@ export class CarrinhoService {
     this.carrinho().reduce((total, item) => total + item.preco, 0)
   );
 
-  adicionar(produto: {nome: string; preco: number}) {
+  carrinhoVazio= computed(() => this.carrinho().length ===0)
+
+  adicionar(produto: ItemCarrinho) {
     this.carrinho.update(listaAtual => [...listaAtual, produto]);
   }
+limpar() {
+  this.carrinho.set([]);
+}
+
 }
--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,32 +1,55 @@
-import { Component,inject } from '@angular/core';
+import { Component,inject,signal } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import {Validators, AbstractControl, ValidationErrors} from '@angular/forms'
 import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,PrecoFormatadoPipe],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class Checkout {
+
   carrinhoService = inject(CarrinhoService);
+
+  compraFinalizada =signal(false);
+
 formulario = new FormGroup({
 nome: new FormControl('',[Validators.required,Validators.minLength(3),nomeSemNumeros]),
 email: new FormControl('',[Validators.required, Validators.email]),
 endereco: new FormControl('',[Validators.required, Validators.minLength(5)]),
 });
+
 finalizar() {
+
+  this.compraFinalizada.set(false);
+
+if(this.carrinhoService.carrinhoVazio()){
+  console.log('Não é possivel finalizar a compra com o carrinho vazio');
+  return;
+}
+
+
+
   if(this.formulario.invalid){
     console.log('Formulário Invalido');
+    this.formulario.markAllAsTouched();
     return;
   }
   const dados = this.formulario.value;
   const itens = this.carrinhoService.itens();
+  const total = this.carrinhoService.totalItens();
 
-
+  console.log('compra finalizada com sucesso!');
 console.log('Dados do formulário:', dados);
 console.log('Itens do carrinho:', itens);
+console.log('Total de compras: ',total)
+
+this.carrinhoService.limpar();
+this.formulario.reset();
+this.compraFinalizada.set(true);
 }
 }
 
